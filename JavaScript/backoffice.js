6db8d17bbdfd4388c7add1844a1c2316974cc978
Daniele-996 <danielesanza@gmail.com>
2025-05-02 23:51:18 +0200

    add

--- JavaScript/backoffice.js
+++ JavaScript/backoffice.js
@@ -1,9 +1,16 @@
 const URL = "https://striveschool-api.herokuapp.com/api/product";
+
 const form = document.querySelector("form");
 console.log(form);
 
-const getObjet = (e) => {
+form.onsubmit = function (e) {
   e.preventDefault();
+
+  const inputName = document.getElementById("name");
+  const inputDesc = document.getElementById("description");
+  const inputBrand = document.getElementById("brand");
+  const inputUrl = document.getElementById("imageUrl");
+  const inputPrice = document.getElementById("price");
 
   const newValue = {
     name: inputName.value,
@@ -13,27 +20,27 @@ const getObjet = (e) => {
     price: inputPrice.value,
   };
 
+  console.log("SUBMIT", newValue);
+  form.reset();
+
   fetch(URL, {
     method: "POST",
     headers: {
+      "Content-Type": "application/json",
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODE0OTM2NTFjMjUwNDAwMTUxYWI2YmEiLCJpYXQiOjE3NDYxODAxNDIsImV4cCI6MTc0NzM4OTc0Mn0.gqHgm9oMRMV6a9xQfr0aPRASQQqUZKE5GrJFP4pK6qE",
     },
     body: JSON.stringify(newValue),
   })
-    .then((obj) => {
-      form.innerText = "";
-      alert("Hai creato un nuovo prodotto!");
-      inputName.value = "";
-      inputDesc.value = "";
-      inputBrand.value = "";
-      inputUrl.value = "";
-      inputPrice.value = "";
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Errore nella fetch");
+      }
+
+      return resp.json();
     })
     .catch((error) => {
       alert("Errore durante la creazione del prodotto!");
       console.error(error);
     });
 };
-
-form.addEventListener("submit", getObjet);
---
+++ JavaScript/homepage.js
@@ -0,0 +1,109 @@
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
+const row = document.getElementById("newRow");
+
+const newCard = () => {
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODE0OTM2NTFjMjUwNDAwMTUxYWI2YmEiLCJpYXQiOjE3NDYxODAxNDIsImV4cCI6MTc0NzM4OTc0Mn0.gqHgm9oMRMV6a9xQfr0aPRASQQqUZKE5GrJFP4pK6qE",
+    },
+    body: JSON.stringify(),
+  })
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Errore nella fetch");
+      }
+
+      return resp.json();
+    })
+    .then((object) => {
+      row.innerText = "";
+      object.forEach((newObj) => {
+        console.log(newObj);
+
+        const col = document.createElement("div");
+        col.className = "col-12 col-md-6 col-lg-4 mb-4";
+
+        const colCard = document.createElement("div");
+        colCard.className = "card-4 shadow p-1 mb-5 bg-body-tertiary rounded";
+
+        const img = document.createElement("img");
+        img.className = "bd-placeholder-img card-img-top cardImg";
+        img.src = newObj.imageUrl;
+        img.alt = newObj.description;
+
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body";
+
+        const h5 = document.createElement("h5");
+        h5.className = "card-title";
+        h5.innerText = newObj.name;
+
+        const p = document.createElement("p");
+        p.className = "card-text text-truncate";
+        p.innerText = newObj.description;
+
+        const brand = document.createElement("p");
+        brand.className = "card-text text-truncate";
+        brand.innerText = newObj.brand;
+
+        const price = document.createElement("p");
+        price.className = "card-text text-truncate";
+        price.innerText = newObj.price + "€";
+
+        const divBtn = document.createElement("div");
+        divBtn.className = "d-flex justify-content-between align-items-center";
+
+        const divBtnGroup = document.createElement("div");
+        divBtnGroup.className = "btn-group";
+
+        const btnFirst = document.createElement("a");
+        btnFirst.type = "button";
+        btnFirst.href = "./details.html";
+        btnFirst.className = "btn btn-sm btn-success";
+        btnFirst.innerText = "Details";
+
+        const btnSecond = document.createElement("a");
+        btnSecond.type = "button";
+        btnSecond.className = "btn btn-sm btn-danger";
+        btnSecond.innerText = "Delete";
+
+        divBtnGroup.appendChild(btnFirst);
+        divBtnGroup.appendChild(btnSecond);
+
+        divBtn.appendChild(divBtnGroup);
+
+        cardBody.appendChild(h5);
+        cardBody.appendChild(p);
+        cardBody.appendChild(brand);
+        cardBody.appendChild(price);
+        cardBody.appendChild(divBtn);
+
+        colCard.appendChild(img);
+        colCard.appendChild(cardBody);
+
+        col.appendChild(colCard);
+
+        row.appendChild(col);
+        console.log(newObj._id);
+
+        // fetch(URL + newObj._id, {
+        //   method: "DELETE",
+        //   headers: {
+        //     Authorization:
+        //       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODE0OTM2NTFjMjUwNDAwMTUxYWI2YmEiLCJpYXQiOjE3NDYxODAxNDIsImV4cCI6MTc0NzM4OTc0Mn0.gqHgm9oMRMV6a9xQfr0aPRASQQqUZKE5GrJFP4pK6qE",
+        //   },
+        //   body: JSON.stringify(),
+
+        // });
+      });
+    })
+
+    .catch((error) => {
+      alert("Errore durante la creazione del prodotto!");
+      console.error(error);
+    });
+};
+
+newCard();
